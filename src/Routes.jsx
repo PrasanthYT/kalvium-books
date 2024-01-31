26727ccf6af0ff668f4053124729dfffced87805
Prasanth <141898438+prasanthj2023@users.noreply.github.com>
2024-01-31 17:44:17 +0530

    Updated

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes,Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Register from "./Components/Register";
-import Registered from "./Components/Registered";
 
 function AllRoutes(){
     return(
